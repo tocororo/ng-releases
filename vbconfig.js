e854359e8e09056683ac5440add1c90279839f58
Rafael Martinez Estevez <malayibiri@gmail.com>
2022-11-07 12:30:46 -0500

    new test

--- vbconfig.js
+++ vbconfig.js
@@ -5,7 +5,7 @@
  * Thus if VocBench3 and Semantic Turkey are running on the same machine this variable can be left commented,
  * otherwise uncomment the line and edit the value.
  */
-// var st_host = "semanticturkey.sceiba.cu";
+var st_host = "vocabularios.sceiba.org";
 
 /**
  * Port of the container hosting Semantic Turkey.
@@ -14,7 +14,7 @@
  * Thus if VocBench3 and Semantic Turkey are running on the same container this variable can be left commented,
  * otherwise uncomment the line and edit the value.
  */
-//var st_port = "8443";
+var st_port = "443";
 
 /**   
  * Path where SemanticTurkey server is listening. If omitted, the sole host is considered.
@@ -23,15 +23,24 @@
  *  This additional path information is considered to be the starting part of the path described above, 
  *  and is usually necessary in case Semantic Turkey is installed behind a proxy redirecting the ST URL.
  */ 
-//var st_path;
+var st_path = "st";
 
 /**
  * Protocol - either http or https.
  * By default (variable left unspecified) the protocol is resolved dynamically by using the same one of the
  * container hosting VocBench.
  */
-//var st_protocol = "http";
+// var st_protocol = "https";
 
-st_guest_user='guest@sceiba.cu'
-st_guest_pass='password'
-  
+/**
+ * A list of l10n supported languages in addition to those already factory provided.
+ * In order to add the support for a language you need to add the <langTag>.json translation file to
+ * the folder assets/l10n and then add the same langTag to the following list
+ */
+var additional_l10n_langs = [];
+
+/**
+ * In case of SAML login enabled, this will the label of the login button.
+ * If not provided, the default label is "SAML Login"
+ */
+// var saml_login_label = "Login"
